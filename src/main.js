69f7500dbb5ad80eb475b351d20d1aeb47702af3
xiwang <xiw@xiw.com>
2023-11-16 10:37:05 +0800

    删除 testAPI.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,9 @@ import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import {getCategory} from '@/apis/testAPI'
 import '@/styles/common.scss'
 
-getCategory().then(res => {
-    console.log(res)
-})
+
 
 const app = createApp(App)
 
