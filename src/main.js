d6848f7c0bc87e18202b7a0a77ac6d7825dbbf33
xiwang <xiw@xiw.com>
2023-11-16 13:46:36 +0800

    图片懒加载指令

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'
-
+import {useIntersectionObserver} from "@vueuse/core";
 
 
 const app = createApp(App)
@@ -15,3 +15,18 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        console.log(el, binding.value)
+
+        useIntersectionObserver(
+            el,
+            ([{isIntersecting}]) => {
+                if (isIntersecting) {
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+})
