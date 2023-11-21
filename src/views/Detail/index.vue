4d61af5d87c8cf8496d0cdaf3db235b6ac43cc0d
xiwang <xiw@xiw.com>
2023-11-21 14:29:43 +0800

    详情基础数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,19 +1,30 @@
 <script setup>
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+import {getDetail} from "../../apis/detail";
 
+const good = ref({})
+const getGoodDetail = async () => {
+  const route = useRoute()
+  const res = await getDetail(route.params.id);
+  good.value = res.result
+}
+
+onMounted(() => getGoodDetail())
 
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="good.details">
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
+          <el-breadcrumb-item :to="{ path: `/category/${good.categories[1].id}` }">{{ good.categories[1].name }}
           </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
+          <el-breadcrumb-item :to="{ path: `/category/sub/${good.categories[0].id}` }">{{ good.categories[0].name }}
           </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ good.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -27,33 +38,33 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p> {{good.salesCount}} </p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{good.commentCount}}</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ good.collectCount}}</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{good.brand.name}}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name"> {{good.name}} </p>
+              <p class="g-desc">{{good.desc}} </p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{good.oldPrice}}</span>
+                <span> {{ good.price}}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -93,13 +104,13 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in good.details.properties" :key="item.value">
+                      <span class="dt">{{item.name}}</span>
+                      <span class="dt">{{item.value}}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                  <img v-for="img in good.details.pictures" :src="img" :key="img" alt="">
                 </div>
               </div>
             </div>
@@ -252,7 +263,7 @@
       flex: 1;
       position: relative;
 
-      ~li::after {
+      ~ li::after {
         position: absolute;
         top: 10px;
         left: 0;
@@ -306,7 +317,7 @@
       font-size: 18px;
       position: relative;
 
-      >span {
+      > span {
         color: $priceColor;
         font-size: 16px;
         margin-left: 10px;
@@ -340,7 +351,7 @@
     }
   }
 
-  >img {
+  > img {
     width: 100%;
   }
 }
