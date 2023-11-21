ec07483012c189ca9f31f39ed960f517cad2eeeb
xiwang <xiw@xiw.com>
2023-11-21 16:12:26 +0800

    放大镜效果实现

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 // 图片列表
-import {ref} from "vue";
+import {ref, watch} from "vue";
+import {useMouseInElement} from "@vueuse/core";
 
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -14,16 +15,45 @@ const activeIndex = ref(0)
 const enterHandler = (i) => {
   activeIndex.value = i
 }
+
+const target = ref(null)
+
+const {elementX, elementY, isOutside} = useMouseInElement(target)
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY], () => {
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100
+  }
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100
+  }
+  if (elementX.value > 300) {
+    left.value = 200
+  }
+  if (elementX.value < 100) {
+    left.value = 0
+  }
+  if (elementY.value > 300) {
+    top.value = 200
+  }
+  if (elementY.value < 100) {
+    top.value = 0
+  }
+})
 </script>
 
 
 <template>
+  {{ elementX }}
+  {{ elementY }}
+  {{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt=""/>
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
