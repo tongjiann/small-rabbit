23ecf5e5f7cc5af202f59cd961794976a5dd7d67
xiwang <xiw@xiw.com>
2023-11-22 14:15:15 +0800

    列表购物车-单选功能实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -20,12 +20,20 @@ export const useCartStore = defineStore('cart', () => {
         const totalCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
 
         const totalPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+        const singleCheck = (skuId, selected) => {
+            // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+            const item = cartList.value.find((item) => item.skuId === skuId)
+            item.selected = selected
+        }
+
         return {
             cartList,
             addCart,
             delCart,
             totalCount,
-            totalPrice
+            totalPrice,
+            singleCheck
         }
     },
     {
