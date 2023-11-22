016196e3addc68092f508b918aa3f168f1fd19f3
xiwang <xiw@xiw.com>
2023-11-22 09:27:15 +0800

    piniaPluginPersistedstate持久化数据

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ import router from './router'
 import '@/styles/common.scss'
 import {lazyPlugin} from "@/directives";
 import {componentPlugin} from "./components";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.use(lazyPlugin)
